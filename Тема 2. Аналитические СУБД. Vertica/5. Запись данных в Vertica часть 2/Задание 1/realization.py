7a56f8ef953e6635b89ddb3ce5693408254f9579
NikMaNik <nikolay.m96@mail.ru>
2025-02-23 20:53:52 +0500

    add

--- Тема 2. Аналитические СУБД. Vertica/5. Запись данных в Vertica часть 2/Задание 1/realization.py
+++ Тема 2. Аналитические СУБД. Vertica/5. Запись данных в Vertica часть 2/Задание 1/realization.py
@@ -1,1 +1,17 @@
-vertica_user = 'укажите здесь свой логин'
+vertica_user = 'stv2025021816'
+
+import csv 
+from pathlib import Path
+
+dataset = 'test_dataset.csv'
+N = 10000 
+
+with open (dataset, 'w') as csvfile:
+    fwriter = csv.writer(csvfile, delimiter = '|')
+    for i in range(N):
+        fwriter.writerow([i, 'asds'])
+print(Path(dataset).resolve())
+
+with open(dataset, 'r') as csvfile:
+    for i in range(5):
+        print(csvfile.readline(), end = '')
