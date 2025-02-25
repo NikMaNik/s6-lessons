e5265685929cf4d0febacd0b5184b21b16a62f92
NikMaNik <nikolay.m96@mail.ru>
2025-02-25 23:24:15 +0500

    add

--- Тема 2. Аналитические СУБД. Vertica/5. Запись данных в Vertica часть 2/Задание 2/members.sql
+++ Тема 2. Аналитические СУБД. Vertica/5. Запись данных в Vertica часть 2/Задание 2/members.sql
@@ -1,4 +1,4 @@
-CREATE TABLE members
+CREATE TABLE stv2025021816.members
 (
     id int NOT NULL,
     age int,
@@ -6,3 +6,9 @@ CREATE TABLE members
     email varchar(50),
     CONSTRAINT C_PRIMARY PRIMARY KEY (id) DISABLED
 );
+
+copy stv2025021816.members (xx, cc , vv ENRFORCELENGTH ) 
+from local '/home/nikolay/python_projects/practicum_yandex/s6-lessons/Тема 2. Аналитические СУБД. Vertica/5. Запись данных в Vertica часть 2/Задание 2/members.csv';
+DELIMITER ';'
+REJECTED DATA AS TABLE stv2025021816.members_rej;
+;
