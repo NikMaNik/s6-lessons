804e2516e36e63ca163fda7f56e8bdfca6496006
NikMaNik <nikolay.m96@mail.ru>
2025-03-02 03:54:13 +0500

    add

--- Тема 2. Аналитические СУБД. Vertica/11. Удаление и обновление данных в Vertica/Задание 1/realization.sql
+++ Тема 2. Аналитические СУБД. Vertica/11. Удаление и обновление данных в Vertica/Задание 1/realization.sql
@@ -1,6 +1,6 @@
 SET SESSION AUTOCOMMIT TO off;
 
-DELETE FROM STV2025021816.members WHERE age > 45 ;
+DELETE FROM STV2025021816.members WHERE age >= 45 ;
 
 SELECT node_name, projection_name, deleted_row_count FROM DELETE_VECTORS
     where projection_name like 'members%';
