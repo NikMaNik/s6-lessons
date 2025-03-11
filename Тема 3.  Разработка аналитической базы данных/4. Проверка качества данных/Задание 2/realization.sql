3a45f7ce5fbd810157d9c4851e2bde4cc5dcd9d6
NikMaNik <nikolay.m96@mail.ru>
2025-03-11 23:45:12 +0500

    add

--- Тема 3.  Разработка аналитической базы данных/4. Проверка качества данных/Задание 2/realization.sql
+++ Тема 3.  Разработка аналитической базы данных/4. Проверка качества данных/Задание 2/realization.sql
@@ -1,17 +1,20 @@
 SELECT 
     COUNT(*) as total,
-    COUNT(DISTINCT id) as uniq
+    COUNT(DISTINCT id) as uniq,
+    'users' as dataset
 FROM 
     STV2025021816__STAGING.users
 UNION ALL 
 SELECT 
     COUNT(*) as total,
-    COUNT(DISTINCT id) as uniq
+    COUNT(DISTINCT id) as uniq,
+    'groups' as dataset
 FROM 
     STV2025021816__STAGING.groups
 UNION ALL
 SELECT 
     COUNT(*) as total,
-    COUNT(DISTINCT message_id) as uniq
+    COUNT(DISTINCT message_id) as uniq,
+    'dialogs' as dataset
 FROM
     STV2025021816__STAGING.dialogs;
